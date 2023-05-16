ea4b505c92c4e13a6414edcfaab45137fe367bf0
sammyremi <adebayorsamuelas@gmail.com>
2023-05-16 17:43:03 +0100

    modify restaurant in app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "myyelp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "myyelp6724e8d2": {
       "AppClientID": "string",
--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
+import { API } from "aws-amplify";
 import {
-  withAuthenticator,
   Button,
+  Flex,
   Heading,
-  Image,
+  Text,
+  TextField,
   View,
-  Card,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
+import { listRestaurants } from "./graphql/queries";
+import {
+  createRestaurant as createRestaurantMutation,
+  deleteRestaurant as deleteRestaurantMutation,
+} from "./graphql/mutations";
 
-function App({ signOut }) {
+const App = ({ signOut }) => {
+  const [rests, setRests] = useState([]);
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  async function fetchRestaurants() {
+    const apiData = await API.graphql({ query: listRestaurants });
+    const restsFromAPI = apiData.data.listRestaurants.items;
+    setRests(restsFromAPI);
+  }
+
+  async function createRestaurant(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+    };
+    await API.graphql({
+      query: createRestaurantMutation,
+      variables: { input: data },
+    });
+    fetchRestaurants();
+    event.target.reset();
+  }
+
+  async function deleteRestaurant({ id }) {
+    const newRestaurants = rests.filter((rest) => rest.id !== id);
+    setRests(newRestaurants);
+    await API.graphql({
+      query: deleteRestaurantMutation,
+      variables: { input: { id } },
+    });
+  }
+
   return (
     <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+      <Heading level={1}>My Restaurants App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createRestaurant}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Restaurants Name"
+            label="Restaurants Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Restaurants Description"
+            label="Restaurants Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Restaurant
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Available Restaurants</Heading>
+      <View margin="3rem 0">
+        {rests.map((rest) => (
+          <Flex
+            key={rest.id || rest.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {rest.name}
+            </Text>
+            <Text as="span">{rest.description}</Text>
+            <Button variation="link" onClick={() => deleteRestaurant(rest)}>
+              Delete Restaurant
+            </Button>
+          </Flex>
+        ))}
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
-}
+};
 
 export default withAuthenticator(App);
